55504fc40b7170aafe2c9f71d4bbba51e90f304b
Daniel Quackenbush <thequack45@gmail.com>
2015-10-16 18:05:12 -0400

    Original Commit

---
+++ HypeBotCSharp/IrcConnectionDialog.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HypeBotCSharp
+{
+    /// <summary>
+    /// Interaction logic for IrcConnectionDialog.xaml
+    /// </summary>
+    public partial class IrcConnectionDialog : Window
+    {
+        private string ircHostname;
+        private string ircNick;
+        private string ircPass = null;
+        public IrcConnectionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ircConnectSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.ircHostname = this.ircConnectHostTextBox.Text;
+            MainWindow.ircNick = this.ircConnectNickTextBox.Text;
+            if ((bool)this.ircConnectUsePassCheckBox.IsChecked)
+            {
+                MainWindow.ircPass = ircConnectPassTextBox.Text;
+            }
+
+            this.Close();
+        }
+
+        private void ircConnectUsePassCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ircConnectPassTextBox.IsEnabled = true;
+        }
+
+        private void ircConnectUsePassCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ircConnectPassTextBox.IsEnabled = false;
+        }
+    }
+}
---
+++ HypeBotCSharp/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HypeBotCSharp;
+using ChatSharp;
+using ChatSharp.Events;
+
+namespace HypeBotCSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static IrcClient ircClient;
+        public static IrcUser ircConnectionUser;
+        public static string ircHostname;
+        public static string ircNick;
+        public static string ircPass = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string commandText = cmdInputTextBox.Text;
+            string[] commandList = Regex.Split(commandText, " ");
+
+            
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void mainWindowMenuSetupDropDownConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            IrcConnectionDialog ircConnectResult = new IrcConnectionDialog();
+            ircConnectResult.ShowDialog();
+
+            // Handle connection info dialog
+            if (ircConnectResult != null)
+            {
+                ircConnectResult.Close();
+            }
+            botOutputBox.Document.Blocks.Clear();
+
+            if (ircPass != null)
+            {
+                ircConnectionUser = new IrcUser(ircNick, ircPass);
+            }
+            else
+            {
+                ircConnectionUser = new IrcUser(ircNick);
+            }
+            ircClient = new IrcClient(ircHostname, ircConnectionUser);
+
+            ircClient.ConnectionComplete += (s, e2) => botOutputBox.AppendText("Connection Completed");
+
+            ircClient.RawMessageRecieved += (s, messageReceivedEventArgs) =>
+            {
+                botOutputBox.AppendText(messageReceivedEventArgs.Message.ToString());
+            };
+
+            ircClient.ConnectAsync();
+        }
+
+        private void handleMessage(PrivateMessageEventArgs messageReceivedEventArgs)
+        {
+            
+        }
+    }
+}
